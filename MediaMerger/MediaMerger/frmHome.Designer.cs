ab859ca462b3870136b9830b90b7870f577dd725
sprabhui <75115886+sprabhui@users.noreply.github.com>
2020-12-14 22:11:23 +0530

    first

---
+++ MediaMerger/MediaMerger/frmHome.Designer.cs
@@ -0,0 +1,88 @@
+﻿
+namespace MediaMerger
+{
+    partial class frmHome
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.lstDir = new System.Windows.Forms.ListBox();
+            this.btnLoadDir = new System.Windows.Forms.Button();
+            this.proBar = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // lstDir
+            // 
+            this.lstDir.FormattingEnabled = true;
+            this.lstDir.ItemHeight = 20;
+            this.lstDir.Location = new System.Drawing.Point(8, 67);
+            this.lstDir.Name = "lstDir";
+            this.lstDir.Size = new System.Drawing.Size(620, 304);
+            this.lstDir.TabIndex = 0;
+            // 
+            // btnLoadDir
+            // 
+            this.btnLoadDir.Location = new System.Drawing.Point(8, 39);
+            this.btnLoadDir.Name = "btnLoadDir";
+            this.btnLoadDir.Size = new System.Drawing.Size(166, 29);
+            this.btnLoadDir.TabIndex = 1;
+            this.btnLoadDir.Text = "Load Directories";
+            this.btnLoadDir.UseVisualStyleBackColor = true;
+            this.btnLoadDir.Click += new System.EventHandler(this.btnLoadDir_Click);
+            // 
+            // proBar
+            // 
+            this.proBar.Location = new System.Drawing.Point(8, 378);
+            this.proBar.Name = "proBar";
+            this.proBar.Size = new System.Drawing.Size(620, 18);
+            this.proBar.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.proBar.TabIndex = 2;
+            this.proBar.UseWaitCursor = true;
+            // 
+            // frmHome
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(642, 418);
+            this.Controls.Add(this.proBar);
+            this.Controls.Add(this.btnLoadDir);
+            this.Controls.Add(this.lstDir);
+            this.Name = "frmHome";
+            this.Text = "Home";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.ListBox lstDir;
+        private System.Windows.Forms.Button btnLoadDir;
+        private System.Windows.Forms.ProgressBar proBar;
+    }
+}
+
---
+++ MediaMerger/MediaMerger/frmHome.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MediaMerger
+{
+    public partial class frmHome : Form
+    {
+        public frmHome()
+        {
+            InitializeComponent();
+            proBar.Value = 0;
+        }
+
+        private void btnLoadDir_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.ShowDialog();
+            if(folderBrowserDialog1.SelectedPath != null)
+            {
+                lstDir.Items.Clear();
+                loadDirectories(folderBrowserDialog1.SelectedPath.ToString());
+            }
+        }
+
+        private void loadDirectories(string dirpath)
+        {
+            int i = 0,j=0;
+            try
+            {
+                // Set a variable to the My Documents path.
+                string docPath = dirpath;
+
+                List<string> dirs = new List<string>(Directory.EnumerateDirectories(docPath));
+
+                j = 100 / dirs.Count;
+                MessageBox.Show("" + j);
+                i = j;
+                foreach (var dir in dirs)
+                {
+                    lstDir.Items.Add(dir);
+                    i = i + j;
+                    proBar.Value = i > 100? 100:i;
+                   // lstDir.Items.Add($"{dir.Substring(dir.LastIndexOf(Path.DirectorySeparatorChar) + 1)}");
+                }
+                Console.WriteLine($"{dirs.Count} directories found.");
+                MessageBox.Show("" + dirs.Count);
+            }
+            catch (UnauthorizedAccessException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (PathTooLongException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
